d8b56a09dcc8b6a9ce6cb5bdbdc8aad20bf74b15
Martin Löper <martin.loeper@student.kit.edu>
2017-07-06 15:13:16 +0200

    Remove files which are auto built by MWE2

--- edu.kit.ipd.sdq.kamp.ruledsl/bin/edu/kit/ipd/sdq/kamp/ruledsl/scoping/KampRuleLanguageGlobalScopeProvider.xtend
+++
@@ -1,7 +0,0 @@
-package edu.kit.ipd.sdq.kamp.ruledsl.scoping
-
-import tools.vitruv.dsls.mirbase.scoping.MirBaseGlobalScopeProvider
-
-class KampRuleLanguageGlobalScopeProvider extends MirBaseGlobalScopeProvider {
-	
-}
--- edu.kit.ipd.sdq.kamp.ruledsl/xtend-gen/edu/kit/ipd/sdq/kamp/ruledsl/scoping/KampRuleLanguageGlobalScopeProvider.java
+++
@@ -1,7 +0,0 @@
-package edu.kit.ipd.sdq.kamp.ruledsl.scoping;
-
-import tools.vitruv.dsls.mirbase.scoping.MirBaseGlobalScopeProvider;
-
-@SuppressWarnings("all")
-public class KampRuleLanguageGlobalScopeProvider extends MirBaseGlobalScopeProvider {
-}
